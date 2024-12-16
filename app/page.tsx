249fed0fefd558595c0a4d0a979225141f19fd86
Joshua Tenorio <joshliam09@gmail.com>
2024-12-15 17:20:12 -0700

    move badge layer

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,35 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center py-24 space-y-4 px-4">
-          <Image src={'/glassypdm-white.png'} width={360} height={90} alt="glassyPDM"/>
-          <div className="grid grid-cols-2">
-        <div className="text-center w-48 self-center justify-self-center">Product Data Management software that respects your filesystem and gets out of your way.</div>
-          <Image src={'/isometricproject.png'} width={500} height={300} alt="projectpage"/>
-          </div>
+        <Image
+          src={"/glassypdm-white.png"}
+          width={360}
+          height={90}
+          alt="glassyPDM"
+        />
         <div className="flex flex-row space-x-16">
           <Badge variant={"outline"}>Open Source</Badge>
           <Badge variant={"outline"}>Self Hosted</Badge>
         </div>
+        <div className="grid grid-cols-2">
+          <div className="text-center w-48 self-center justify-self-center">
+            Product Data Management software that respects your filesystem and
+            gets out of your way.
+          </div>
+          <Image
+            src={"/isometricproject.png"}
+            width={500}
+            height={300}
+            alt="projectpage"
+          />
+        </div>
+
         <Button variant={"default"}>
-          <a target="_blank" href='https://github.com/glassypdm/glassypdm-client/releases/latest'>
-          Download on GitHub
+          <a
+            target="_blank"
+            href="https://github.com/glassypdm/glassypdm-client/releases/latest"
+          >
+            Download on GitHub
           </a>
         </Button>
       </div>
@@ -27,7 +44,12 @@ export default function Home() {
         <div>Trusted by</div>
         <div className="flex flex-row space-x-4">
           <Link href={"https://fsae.engineering.asu.edu/"} target="_blank">
-          <Image src={'/SDMLogo.png'} width={250} height={200} alt="Sun Devil Motorsports"/>
+            <Image
+              src={"/SDMLogo.png"}
+              width={250}
+              height={200}
+              alt="Sun Devil Motorsports"
+            />
           </Link>
         </div>
       </div>
